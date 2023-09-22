efaa990a99a2751b18241c4be640334fe469b6cd
giepiepau <144468738+giepiepau@users.noreply.github.com>
2023-09-21 22:20:13 -0400

    add space to clear error

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -4,6 +4,7 @@ import LaptopImg from "../../assets/home-main.svg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
+  
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
